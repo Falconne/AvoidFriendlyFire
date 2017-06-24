b55416fdce2af6e1344bea9a4c3204f70fd310dd
Anuradha Dissanayake <falconne@gmail.com>
2017-06-25 00:35:43 +1200

    Add empty project

---
+++ Main.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvoidFriendlyFire
+{
+    public class Main : HugsLib.ModBase
+    {
+        public Main()
+        {
+            Instance = this;
+        }
+
+        public override string ModIdentifier => "AvoidFriendlyFire";
+
+        internal static Main Instance { get; private set; }
+    }
+}
