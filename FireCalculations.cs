1124d0b722220d55f721fba1dfbc9ae3c427d7f3
Anuradha Dissanayake <falconne@gmail.com>
2017-07-11 16:20:04 +1200

    Add FireCalculations WIP

---
+++ FireCalculations.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using Verse;
+
+namespace AvoidFriendlyFire
+{
+    public class FireCalculations
+    {
+        public static void BuildFireCone(IntVec3 origin, IntVec3 target)
+        {
+            var result = new HashSet<int>();
+            
+            result.Clear();
+
+            Map map = Find.VisibleMap;
+
+            if (!target.InBounds(map) || target.Fogged(map))
+                return;
+
+            if (target == origin)
+                return;
+
+            for (var xSplash = target.x - 1; xSplash <= target.x + 1; xSplash++)
+            {
+                for (var zSplash = target.z - 1; zSplash <= target.z + 1; zSplash++)
+                {
+                    var splashTarget = new IntVec3(xSplash, target.y, zSplash);
+                    result.UnionWith(GetShootablePointsBetween(origin, splashTarget, map));
+                }
+            }
+        }
+
+        private static IEnumerable<int> GetShootablePointsBetween(
+            IntVec3 source, IntVec3 target, Map map)
+        {
+            foreach (IntVec3 point in GenSight.PointsOnLineOfSight(source, target))
+            {
+                if (!point.CanBeSeenOver(map))
+                    yield break;
+
+                yield return map.cellIndices.CellToIndex(point.x, point.z);
+            }
+
+            yield return map.cellIndices.CellToIndex(target.x, target.z);
+        }
+    }
+}
