e33769da19e09cf9a902415201c2db6ec50ff804
Anuradha Dissanayake <falconne@gmail.com>
2017-07-30 11:19:34 +1200

    Adding FireManager WIP

---
+++ CachedFireCone.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Verse;
+
+namespace AvoidFriendlyFire
+{
+    public class CachedFireCone
+    {
+        public CachedFireCone(HashSet<int> fireCone)
+        {
+            FireCone = fireCone;
+            _expireAt = Find.TickManager.TicksGame + 1000;
+        }
+
+        public bool IsExpired()
+        {
+            return Find.TickManager.TicksGame >= _expireAt;
+        }
+
+        public readonly HashSet<int> FireCone;
+
+        private readonly int _expireAt;
+    }
+}
--- FireManager.cs
+++ FireManager.cs
@@ -1,12 +1,16 @@
-﻿using Verse;
+﻿using System.Collections.Generic;
+using Verse;
 
 namespace AvoidFriendlyFire
 {
     public class FireManager
     {
-        bool CanHitTarget(Pawn source, Pawn target)
+        bool CanHitTargetSafely(Pawn source, Pawn target)
         {
             
         }
+
+        private readonly Dictionary<int, Dictionary<int, CachedFireCone>> _cachedFireCones
+            = new Dictionary<int, Dictionary<int, CachedFireCone>>();
     }
 }
