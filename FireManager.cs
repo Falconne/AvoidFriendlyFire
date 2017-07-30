7d71a0410d537474b3f92c3e4d104e414e2a594b
Anuradha Dissanayake <falconne@gmail.com>
2017-07-30 16:06:39 +1200

    Adding FireManager WIP

--- FireManager.cs
+++ FireManager.cs
@@ -5,9 +5,13 @@ namespace AvoidFriendlyFire
 {
     public class FireManager
     {
-        bool CanHitTargetSafely(Pawn source, Pawn target)
+        bool CanHitTargetSafely(IntVec3 origin, IntVec3 target)
         {
-            
+            var map = Find.VisibleMap;
+            var originIndex = map.cellIndices.CellToIndex(origin);
+            var targetIndex = map.cellIndices.CellToIndex(target);
+
+            if (_cachedFireCones.TryGetValue(originIndex, out var cachedFireConesFromOrigin))
         }
 
         private readonly Dictionary<int, Dictionary<int, CachedFireCone>> _cachedFireCones
