2be9c4dca76085e8794edaa9c7e1a61e6868c21e
Anuradha Dissanayake <falconne@gmail.com>
2017-09-17 19:09:54 +1200

    Fix bug with pawns not putting out fires when friendlies are standing next to them.

--- Patches/Verb_CanHitTargetFrom_Patch.cs
+++ Patches/Verb_CanHitTargetFrom_Patch.cs
@@ -6,12 +6,19 @@ namespace AvoidFriendlyFire
     [HarmonyPatch(typeof(Verb), "CanHitTargetFrom")]
     public class Verb_CanHitTargetFrom_Patch
     {
-        public static void Postfix(ref Verb __instance, ref bool __result, IntVec3 root, LocalTargetInfo targ)
+        public static void Postfix(ref Verb __instance, ref bool __result, IntVec3 root, 
+            LocalTargetInfo targ)
         {
             if (!Main.Instance.IsModEnabled())
                 return;
 
             if (!__result || !__instance.CasterIsPawn || !targ.IsValid)
+                return;
+
+            if (targ.Thing != null && targ.Thing == __instance.caster)
+                return;
+
+            if (root.DistanceTo(targ.Cell) <= 2f)
                 return;
 
             var pawn = __instance.CasterPawn;
