c71a99857e536a5b60ee3451f735e48469e47dbb
Anuradha Dissanayake <falconne@gmail.com>
2019-03-15 23:46:28 +1300

    Friendly animals who are blocking shooters will have their name label shown in green, even if animal labels are off.

---
+++ src/AvoidFriendlyFire/Patches/PawnUIOverlay_DrawPawnGUIOverlay_Patch.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using Harmony;
+using UnityEngine;
+using Verse;
+
+namespace AvoidFriendlyFire
+{
+    [HarmonyPatch(typeof(PawnUIOverlay), "DrawPawnGUIOverlay")]
+    public class PawnUIOverlay_DrawPawnGUIOverlay_Patch
+    {
+        private static readonly FieldInfo _pawnGetter =
+            typeof(PawnUIOverlay).GetField("pawn", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        public static bool Prefix(ref PawnUIOverlay __instance)
+        {
+            var pawn = (Pawn) _pawnGetter.GetValue(__instance);
+            if (!pawn.Spawned || pawn.Map.fogGrid.IsFogged(pawn.Position))
+            {
+                return false;
+            }
+
+            if (pawn.RaceProps.Humanlike || pawn.Name == null)
+                return true;
+
+            if (!Main.Instance.PawnStatusTracker.IsABlocker(pawn))
+                return true;
+
+            Vector2 pos = GenMapUI.LabelDrawPosFor(pawn, -0.6f);
+            GenMapUI.DrawPawnLabel(pawn, pos);
+
+            return false;
+        }
+    }
+}
